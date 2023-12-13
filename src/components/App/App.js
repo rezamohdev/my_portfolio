9dee7a2b8cf51fd8ff869f6fee665fe7618545d6
rezamohdev <mohammadireza29@gmail.com>
2023-12-12 20:09:46 -0500

    feat: install fontawsome

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import logo from '../../logo.svg';
 import './App.css';
 import Button from '../Button/Button';
+import SlideButton from '../SlideButton/SlideButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button lightButton={true}>Hello</Button>
+        <Button lightButton={true}>	&#8592;	</Button>
+        <Button lightButton={true}>	&#8594;</Button>
+        <SlideButton />
         <Button >Eol</Button>
       </header>
     </div>
---
+++ src/components/SlideButton/SlideButton.js
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './SlideButton.css';
+const SlideButton = ({ name }) => {
+    return (<button className='slider-button'><FontAwesomeIcon size='xl' icon="coffee" /></button>)
+}
+export default SlideButton;
