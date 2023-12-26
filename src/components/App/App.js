7432131843326a35ef3eac5686685a20b7cbe1db
rezamohdev <mohammadireza29@gmail.com>
2023-12-25 20:21:19 -0500

    Cover

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,11 +4,13 @@ import Button from '../Button/Button';
 import SlideButton from '../SlideButton/SlideButton';
 import { GalleryCard } from '../GalleryCard/GalleryCard';
 import Header from '../Header/Header';
+import Cover from '../Cover/Cover';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Cover />
     </div>
   );
 }
---
+++ src/components/Cover/Cover.js
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import Button from '../Button/Button';
+import './Cover.css';
+import CoverPhotoSrc from '../../images/MyPhoto.png';
+const Cover = () => {
+    return (<div className='cover'>
+        <div className='cover__info'>
+            <span className='cover__welcome'>Welcome to my Portfolio</span>
+            <h1 className='cover__title'>
+                Hi I’m
+                <br />
+                <span style={{ color: '#07f' }}>Reza Mohammadi</span>
+                <br />
+                React Developer
+            </h1>
+            <p className='cover__paragraph'>
+                Collaborating with highly skilled individuals, our agency delivers top-quality services.
+            </p>
+            <div className='cover__buttons'>
+                <a href='https://www.linkedin.com/in/rmoh22/' target='_blank'><Button lightButton={true}>Hire Me!</Button></a>
+                <Button >Download My CV <FontAwesomeIcon icon={faArrowDown} /></Button   >
+            </div>
+        </div>
+        <img src={CoverPhotoSrc} className='cover__photo' />
+    </div>)
+}
+
+export default Cover;
