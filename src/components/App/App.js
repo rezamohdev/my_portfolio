e51076a9b6bbec3a25fac56ad95b92c920b19fdb
rezamohdev <mohammadireza29@gmail.com>
2023-12-26 21:16:11 -0500

    Skills

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,14 @@ import SlideButton from '../SlideButton/SlideButton';
 import { GalleryCard } from '../GalleryCard/GalleryCard';
 import Header from '../Header/Header';
 import Cover from '../Cover/Cover';
+import Skills from '../Skills/Skills';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Cover />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown, faPen, faSchool } from '@fortawesome/free-solid-svg-icons'
+import "./Skills.css";
+import Button from "../Button/Button";
+import skillsPhotoSrc from '../../images/MyPhoto.png';
+
+const Skills = () => {
+    return (<div className="skills">
+        <div className='skills__info'>
+            <span className='skills__welcome'>ABOUT ME</span>
+            <h1 className='skills__title'>
+                <span style={{ color: '#07f' }}>4 Years Experience </span>
+                <br />
+                of Web Development
+            </h1>
+            <p className='skills__paragraph'>
+                Hello there! I'm Robert Junior. I specialize in web design and development, and I'm deeply passionate and committed to my craft. With 20 years of experience as a professional graphic designer  </p>
+            <div className='skills__buttons'>
+                <a href='https://www.linkedin.com/in/rmoh22/' target='_blank'><Button lightButton={true}>Main Skills</Button></a>
+                <Button >Education <FontAwesomeIcon icon={faPen} /></Button   >
+            </div>
+        </div>
+        <img src={skillsPhotoSrc} className='skills__photo' />
+    </div>)
+}
+
+export default Skills;
