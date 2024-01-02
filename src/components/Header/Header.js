2b8d1320bd127c0a04720d407086fd94868c965e
rezamohdev <mohammadireza29@gmail.com>
2024-01-01 19:44:58 -0500

    nabbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import Button from "../Button/Button";
 import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-    return (<header className="header">
-        <span className="header__logo"><span style={{ fontSize: 25 }}>💻</span><b>Reza Mohammadi</b></span>
-        <div className="header__links">
-            <a className="header__link">Home</a>
-            <a className="header__link">About Me</a>
-            <a className="header__link">Contact me</a>
-        </div>
-        <a href="mailto:rezamoh847@gmail.com" ><Button>Let's chat</Button></a>
-    </header>)
+    const [showNavbar, isShowNavbar] = useState(false);
+    return (<>
+        {showNavbar ? <header className="header">
+            <span className="header__logo"><span style={{ fontSize: 25 }}>💻</span><b>Reza Mohammadi</b></span>
+            <div className="header__links">
+                <a className="header__link">Home</a>
+                <a className="header__link">About Me</a>
+                <a className="header__link">Contact me</a>
+            </div>
+            <a href="mailto:rezamoh847@gmail.com" ><Button>Let's chat</Button></a>
+        </header> : <div className="nav">
+            <dic className="nav__top">
+                <span className="header__logo"><span style={{ fontSize: 25 }}>💻</span><b>Reza Mohammadi</b></span>
+                <button className="nav__menu-button">
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+            </dic>
+
+            <div className="nav__buttons">
+                <a className="header__link">Home</a>
+                <a className="header__link">About Me</a>
+                <a className="header__link">Contact me</a>
+            </div>
+        </div>}
+    </>)
 }
 
 export default Header;
