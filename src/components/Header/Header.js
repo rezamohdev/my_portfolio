803525f1cd7af1803d0268e0134eefe79df277d8
rezamohdev <mohammadireza29@gmail.com>
2024-01-02 20:19:49 -0500

    responsive nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,32 +3,40 @@ import Button from "../Button/Button";
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useMediaQuery } from 'react-responsive';
 
 const Header = () => {
     const [showNavbar, isShowNavbar] = useState(false);
+    const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
     return (<>
-        {showNavbar ? <header className="header">
-            <span className="header__logo"><span style={{ fontSize: 25 }}>💻</span><b>Reza Mohammadi</b></span>
-            <div className="header__links">
-                <a className="header__link">Home</a>
-                <a className="header__link">About Me</a>
-                <a className="header__link">Contact me</a>
-            </div>
-            <a href="mailto:rezamoh847@gmail.com" ><Button>Let's chat</Button></a>
-        </header> : <div className="nav">
-            <dic className="nav__top">
-                <span className="header__logo"><span style={{ fontSize: 25 }}>💻</span><b>Reza Mohammadi</b></span>
-                <button className="nav__menu-button">
-                    <FontAwesomeIcon icon={faBars} />
-                </button>
-            </dic>
+        {isMobile ?
+            <div className="nav">
+                <dic className="nav__top">
+                    <span className="header__logo"><span style={{ fontSize: 25 }}>💻</span><b>Reza Mohammadi</b></span>
+                    <button className="nav__menu-button">
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                </dic>
 
-            <div className="nav__buttons">
-                <a className="header__link">Home</a>
-                <a className="header__link">About Me</a>
-                <a className="header__link">Contact me</a>
+                {/* <div className="nav__buttons"> */}
+                <ul className="nav__buttons-list">
+                    <li className="nav__list-item"><a className="nav__link">Home</a></li>
+                    <li className="nav__list-item"><a className="nav__link">About Me</a></li>
+                    <li className="nav__list-item"><a className="nav__link">Contact me</a></li>
+                </ul>
+                {/* </div> */}
             </div>
-        </div>}
+            :
+            <header className="header">
+                <span className="header__logo"><span style={{ fontSize: 25 }}>💻</span><b>Reza Mohammadi</b></span>
+                <div className="header__links">
+                    <a className="header__link">Home</a>
+                    <a className="header__link">About Me</a>
+                    <a className="header__link">Contact me</a>
+                </div>
+                <a href="mailto:rezamoh847@gmail.com" ><Button>Let's chat</Button></a>
+            </header>
+        }
     </>)
 }
 
