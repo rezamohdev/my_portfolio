ae843c5916dc1e49720a7e9a994cf3de1d60322e
rezamohdev <mohammadireza29@gmail.com>
2024-03-06 18:13:40 -0500

    skills responsive

--- src/components/TabGroup/TabGroup.js
+++ src/components/TabGroup/TabGroup.js
@@ -24,7 +24,7 @@ const TabGroup = () => {
                 Education
             </Button>
         </div>
-        <div className="tab__contents">
+        <div className="tab__content">
             {
                 active == "skills" ? (
                     <>
