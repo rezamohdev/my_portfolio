f09c4417a74cb3244a8e9ae802e7cb216623e09d
rezamohdev <mohammadireza29@gmail.com>
2024-10-27 10:14:16 -0400

    update: add hover to card

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,16 +3,25 @@ import { Parallax } from "react-scroll-parallax";
 import { ParallaxBanner } from "react-scroll-parallax";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const ProjectCard = ({ title, desc, iconName }) => {
+  const [isCardInfoHidden, setIsCardInfoHidden] = useState(false);
   return (
     <div className="card">
-      <div className="card__header">
-        <h5>{title}</h5>
+      {isCardInfoHidden && (
+        <div className="card__header">
+          <h5>{title}</h5>
+          <FontAwesomeIcon icon={iconName} size="23" />
+        </div>
+      )}
+      {/* <div className="card__body"> */}
+      <div className="card__circle">
         <FontAwesomeIcon icon={iconName} size="23" />
       </div>
-      <div className="card__body"></div>
-      <div className="card__footer">{desc}</div>
+      {/* </div> */}
+
+      <div className="card__footer">{title}</div>
     </div>
   );
 };
