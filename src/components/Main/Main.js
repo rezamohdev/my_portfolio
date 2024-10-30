8016bd34d99f9347677283a37e25c6b7083410e6
rezamohdev <mohammadireza29@gmail.com>
2024-10-29 20:39:48 -0400

    update: skills list done

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,53 +1,52 @@
 import ProjectCard from "../ProjectCard/ProjectCard";
 import "./Main.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import * as Icons from "@fortawesome/free-solid-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 
-// Add all icons to the library so you can use it in your page
-const iconList = Object.keys(Icons)
-  .filter((key) => key !== "fa" && key !== "prefix")
-  .map((icon) => Icons[icon]);
-library.add(fas, far, fab, iconList);
+library.add(fas, far, fab);
 
-// library.add(...iconList);
 const cardData = [
   {
-    title: "Travellian ( an August 2023 code jam)",
+    title: "ReactJS",
     desc: "lorem ipsum dolor",
     iconPrefix: "fab",
     iconName: "react",
     url: "https://community-for-afg.netlify.app/",
+    bg: "#33b1ff",
   },
   {
-    title: "The community website",
+    title: "Nodejs",
     desc: "lorem ipsum dolor",
-    iconPrefix: "fas",
-    iconName: "users",
+    iconPrefix: "fab",
+    iconName: "node-js",
     url: "https://community-for-afg.netlify.app/",
+    bg: "#c5e069",
   },
   {
-    title: "Synthesis workshop",
+    title: "SQL",
     desc: "lorem ipsum dolor",
     iconPrefix: "fas",
-    iconName: "flask",
+    iconName: "database",
     url: "https://synthesis-workshop.com/",
+    bg: "#2758ba",
   },
   {
-    title: "What to wear",
+    title: "JavaScript",
     desc: "lorem ipsum dolor",
-    iconPrefix: "fas",
-    iconName: "shirt",
+    iconPrefix: "fab",
+    iconName: "js",
     url: "https://github.com/rezamohdev/se_project_react",
+    bg: "#ffab0f",
   },
   {
-    title: "About the US. photo gallery",
+    title: "Problem solving",
     desc: "lorem ipsum dolor",
     iconPrefix: "fas",
-    iconName: "images",
+    iconName: "clipboard-question",
     url: "https://rezamohdev.github.io/se_project_aroundtheus/",
+    bg: "#ff4e33",
   },
 ];
 const Main = () => {
@@ -60,6 +59,7 @@ const Main = () => {
             desc={card.desc}
             iconPrefix={card.iconPrefix}
             iconName={card.iconName}
+            backgroundColor={card.bg}
           />
         );
       })}
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -5,18 +5,27 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
-const ProjectCard = ({ title, desc, iconPrefix, iconName }) => {
+const ProjectCard = ({
+  title,
+  desc,
+  iconPrefix,
+  iconName,
+  backgroundColor,
+}) => {
   const [isCardInfoHidden, setIsCardInfoHidden] = useState(false);
   return (
     <div className="card">
       {isCardInfoHidden && (
         <div className="card__header">
           <h5>{title}</h5>
-          <FontAwesomeIcon icon={iconName} size="23" />
+          <FontAwesomeIcon icon={iconName} size="30" />
         </div>
       )}
       {/* <div className="card__body"> */}
-      <div className="card__circle">
+      <div
+        className="card__circle"
+        style={{ backgroundColor: `${backgroundColor}` }}
+      >
         <FontAwesomeIcon icon={[iconPrefix, iconName]} size="23" />
       </div>
       {/* </div> */}
