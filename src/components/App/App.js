2009e3724c483307fff138f2535bf82e0d4d5952
rezamohdev <mohammadireza29@gmail.com>
2024-11-14 20:15:26 -0500

    update: App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,30 @@ import { AnimatePresence } from "framer-motion";
 import { Header } from "../Header/Header.js";
 import { Item } from "../Item/Item";
 import { List } from "../List/List";
+import {Route, Router} from "react-router-dom";
 
 // import { Routes, Route } from "react-router-dom";
 function store({match}) {
     let {id } = match.params;
     const imageHasLoaded = true;
+    return (
+        <>
+            <List selectedId={id} />
+            <AnimatePresence>
+                {
+                    id && imageHasLoaded && <Item id={id} key="item"/>
+                }
+            </AnimatePresence>
+        </>
+    )
 }
 function App() {
   return (
     <div className="App">
       <Header />
+        <Router>
+            <Route path={["/:id", "/"]} component={store} />
+        </Router>
     </div>
   );
 }
